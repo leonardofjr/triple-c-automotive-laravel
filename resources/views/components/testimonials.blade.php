00a11ddd5b3d4298ba89409c229557d0ced285c7
Leo <leonardo.f.jr@gmail.com>
2018-03-12 14:32:31 -0400

    concatenated script file

--- resources/views/components/testimonials.blade.php
+++ resources/views/components/testimonials.blade.php
@@ -1,7 +1,22 @@
 <div class="testimonial-responsive-carousel">
-  <div>Test</div>
-  <div>Test</div>
-  <div>Test</div>
-  <div>Test</div>
-  <div>Test</div>
+  <div>
+    <h3>Cherry Manansala</h3>
+    <p>Carlos saved me an arm,by the end he save my leg too, problems with my back break (leaking) unable to stop on a dime/engine light. I original got an estimate for much much more then what I was willing to pay.  2 thumbs up, QC aprv</p>
+  </div>
+  <div>
+    <h3>Paul Mailloux</h3>
+    <p>Fantastic work and timely too. Great pricing and Carlos is Super</p>
+  </div>
+  <div>
+    <h3>Randy Alas</h3>
+    <p>Amazing automotive service and very competitive prices. Carlos went above and beyond to make sure i was satisfied with my vehical maintenance  and most importantly he  always provides honest recommandations.</p>
+  </div>
+  <div>
+  <h3>Kevin Rey</h3>
+  <p>Very friendly gentleman! Would strongly recommend!</p>
+  </div>
+  <div>
+    <h3>Janice Patera</h3>
+    <p>Not only does he do superb work he goes above and beyond! Best mechanic ever</p>
+  </div>
 </div>
